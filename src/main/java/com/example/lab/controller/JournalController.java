b2c56364601c2d3609d00f59b7f1e1e35e33bfed
ZhabinYaroslav <33040608+YaroslavZhabin@users.noreply.github.com>
2021-03-18 11:00:49 +0300

    Update JournalController.java

--- src/main/java/com/example/lab/controller/JournalController.java
+++ src/main/java/com/example/lab/controller/JournalController.java
@@ -1,4 +1,53 @@
 package com.example.lab.controller;
 
+import com.example.lab.dao.JournalJdbc;
+import com.example.lab.model.Journal;
+import com.example.lab.model.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin
+@RestController
 public class JournalController {
+    private final JournalJdbc journalJdbc;
+
+    public JournalController(JournalJdbc journalJdbc) {
+        this.journalJdbc = journalJdbc;
+    }
+
+    @GetMapping("/journal/student")
+    public List<Journal> getJournalByStudent(@RequestParam(value="student_id") Integer student_id){
+        return journalJdbc.getJournalByStudent(student_id);
+    }
+
+    @GetMapping("/journal/group")
+    public List<Journal> getJournalByGroup(@RequestParam(value="study_group_id") Integer study_group_id){
+        return journalJdbc.getJournalByGroup(study_group_id);
+    }
+
+    @GetMapping("/journal/types")
+    public List<?> getJournalType(){
+        return journalJdbc.getTypeCourse();
+    }
+
+    @GetMapping("/journal/mark")
+    public List<?> getPerformance(){
+        return journalJdbc.getPerformance();
+    }
+
+    @PostMapping("/journal")
+    int newJournal(@RequestBody Journal newJournal) {
+        try{
+            return journalJdbc.UpdateJournal(newJournal);
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+            return -1;
+        }
+    }
+
+    @DeleteMapping("/journal/delete")
+    public void deleteJournal(@RequestParam(value="id") Integer id){
+        journalJdbc.delete(id);
+    }
 }
